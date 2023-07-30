921ed0d74c470d0244e1e279cd16c7634ed4d1e8
Clement Avenel <clement@squadracer.com>
2023-07-30 21:42:34 +0200

    Add locale_ninja gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'application#index'
+  get 'index', to: 'application#index'
+  mount LocaleNinja::Engine => '/locale_ninja'
 end
